deb53b4152cc3f3f74802ba318a10ee6238a4338
Kanika <kanikasunaria@gmail.com>
2025-12-14 02:34:12 +0530

    another sol

---
+++ 231a_tteam.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    int ans=0;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(a+b+c>=2){
+            ans++;
+
+        }
+        
+
+    }
+    cout<<ans<<endl;
+    return 0;
+}
